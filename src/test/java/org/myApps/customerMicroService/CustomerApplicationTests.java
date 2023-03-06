9c7ced1a9cd246535427a0f4907b9d8ff43fec86
leeCoderEng <nouraddine.ait.elhadj@gmail.com>
2023-03-06 22:39:28 +0100

    initial commit

---
+++ src/test/java/org/myApps/customerMicroService/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myApps.customerMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
